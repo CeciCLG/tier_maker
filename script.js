5ee578fdeb805ca5438d627895695a5b7fd9e2a9
CeciCLG <cecilia.lares@bootcamp-upgrade.com>
2024-08-19 18:26:31 +0200

    se pueden agregar imagenes

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-const imageInput = document.querySelector('image-input');
-const imageSection = document.querySelector('selector-items')
+const imageInput = document.querySelector("#image-input");
+const imageSection = document.querySelector("#selector-items");
+
+imageInput.addEventListener("change", (event) => {
+    console.log(event);
+
+    const [file] = event.target.files;
+
+    console.log(file);
+
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = (eventReader) => {
+            const imageItem = document.createElement("img");
+
+            imageItem.src = eventReader.target.result;
+            imageItem.className = "item-image";
+
+            imageSection.appendChild(imageItem)
+        };
+
+
+        reader.readAsDataURL(file);
+    }
+})
