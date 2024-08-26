c02909235ff224adb0437412ea56c4555d41f9aa
CeciCLG <cecilia.lares@bootcamp-upgrade.com>
2024-08-26 23:22:59 +0200

    se crean las funciones handleDragStar, handleDrangEnd y handleCreate

--- script.js
+++ script.js
@@ -1,27 +1,44 @@
 const imageInput = document.querySelector("#image-input");
 const imageSection = document.querySelector("#selector-items");
 
+function handleCreate(src) {
+    const imageItem = document.createElement("img");
+    imageItem.src = src;
+    imageItem.className = "item-image";
+    imageItem.draggable = true;
+
+    imageItem.addEventListener('dragstart', handleDragStar);
+    imageItem.addEventListener('dragend', handleDrangEnd);
+
+    imageSection.appendChild(imageItem);
+}
+
 imageInput.addEventListener("change", (event) => {
-    console.log(event);
-
     const [file] = event.target.files;
-
-    console.log(file);
-
 
     if (file) {
         const reader = new FileReader();
 
         reader.onload = (eventReader) => {
-            const imageItem = document.createElement("img");
 
-            imageItem.src = eventReader.target.result;
-            imageItem.className = "item-image";
-
-            imageSection.appendChild(imageItem)
+            let src = eventReader.target.result;
+            handleCreate(src)
         };
 
 
         reader.readAsDataURL(file);
     }
-})
+});
+
+let draggedElement = null;
+
+let sourcerContainer = null;
+
+function handleDragStar(event) {
+    console.log('dragStar');
+}
+
+function handleDrangEnd(event) {
+    console.log('dragend');
+
+}
